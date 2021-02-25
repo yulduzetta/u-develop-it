2e3596b3e0172fa19aac3dc2bb37fe5c24ec6e31
yulduz <yulduz83@gmail.com>
2021-02-24 22:24:49 -0600

    adds parties schemna and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,11 @@
-CREATE TABLE candidates (
-    id INTEGER PRIMARY KEY, 
+CREATE TABLE [IF NOT EXISTS] candidates (
+    id INTEGER PRIMARY KEY,
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
+);
+CREATE TABLE [IF NOT EXISTS] parties (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
 );
